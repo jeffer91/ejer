838f71e76352286cb55a39f81ec3b1744d4bdc1e
jeffer91 <jeffersonvillarreal91@gmail.com>
2026-06-22 22:08:37 -0500

    Bloque 14: probar acciones importador

--- src/rutinas/rutina.schema.js
+++ src/rutinas/rutina.schema.js
@@ -1,2 +1,5 @@
-export const A = Object.freeze({ X: "x" });
-export function b(){ return A.X; }
+export const PLAN_ACTIONS = Object.freeze({
+  REPLACE_DAY: "reemplazar_dia",
+  ADD_DAY: "crear_dia",
+  NEW_PLAN: "crear_rutina"
+});
