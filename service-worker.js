7d717815d9f1bfd637948f9a6897e92d793e87ff
jeffer91 <jeffersonvillarreal91@gmail.com>
2026-06-25 02:48:17 -0500

    Document service worker placeholder

---
+++ service-worker.js
@@ -0,0 +1,6 @@
+/*
+  Nombre completo: service-worker.js
+  Ruta o ubicación: service-worker.js
+  Función: archivo reservado para la base PWA de FitJeff.
+  Se conecta con: src/app/app.bootstrap.js.
+*/
