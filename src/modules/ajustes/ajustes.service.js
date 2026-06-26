b1d7c0310bb8be09bd04bfa4d3eb0e64010144a2
jeffer91 <jeffersonvillarreal91@gmail.com>
2026-06-26 08:01:52 -0500

    Bloque 4: conectar ajustes con Control corporal

--- src/modules/ajustes/ajustes.service.js
+++ src/modules/ajustes/ajustes.service.js
@@ -3,19 +3,19 @@
   Ruta o ubicación: src/modules/ajustes/ajustes.service.js
 
   Función o funciones:
-    - Leer perfil y objetivo actuales desde el módulo Registro.
+    - Leer perfil y objetivo actuales desde Control corporal.
     - Guardar cambios de altura, fecha de nacimiento y peso objetivo.
     - Permitir reabrir Inicio sin borrar los datos guardados.
 
   Se conecta con:
-    - src/modules/registro/registro.service.js
-    - src/modules/inicio/inicio.validator.js
+    - src/features/control-corporal/registro.service.js
+    - src/features/control-corporal/inicio/inicio.validator.js
     - src/modules/ajustes/ajustes.constants.js
     - src/modules/ajustes/ajustes.controller.js
 */
 
-import { convertirAlturaACm, convertirPesoAKg } from "../inicio/inicio.validator.js";
-import { crearRegistroService } from "../registro/registro.service.js";
+import { convertirAlturaACm, convertirPesoAKg } from "../../features/control-corporal/inicio/inicio.validator.js";
+import { crearRegistroService } from "../../features/control-corporal/registro.service.js";
 import { AJUSTES_STORAGE_KEYS } from "./ajustes.constants.js";
 
 function fechaNacimientoValida(fecha) {
