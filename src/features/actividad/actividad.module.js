5fed0267a25f396029c36a41bd1f5ed83967805e
jeffer91 <jeffersonvillarreal91@gmail.com>
2026-06-28 09:39:08 -0500

    Bloque 11: crear modulo de Actividad

---
+++ src/features/actividad/actividad.module.js
@@ -0,0 +1,21 @@
+import { ACTIVIDAD_ROUTES, esRutaActividad } from "./actividad.routes.js";
+import { crearActividadResumenController } from "./resumen/resumen.controller.js";
+import { crearActividadRegistroController } from "./registro/registro.controller.js";
+
+export { ACTIVIDAD_ROUTES, esRutaActividad };
+
+export function montarPantallaActividad(rutaId, contenedor, opciones = {}) {
+  if (rutaId === ACTIVIDAD_ROUTES.REGISTRO) {
+    const controller = crearActividadRegistroController({
+      alNavegar: opciones.alNavegar
+    });
+    controller.montar(contenedor);
+    return controller;
+  }
+
+  const controller = crearActividadResumenController({
+    alNavegar: opciones.alNavegar
+  });
+  controller.montar(contenedor);
+  return controller;
+}
